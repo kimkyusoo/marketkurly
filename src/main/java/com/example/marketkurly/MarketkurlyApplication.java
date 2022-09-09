93682124ff8a51af55bca6b2ca377a483d4a3aeb
kyusoo Kim <rbtn110@gmail.com>
2022-09-09 10:40:23 +0900

    기본 빌드.

---
+++ src/main/java/com/example/marketkurly/MarketkurlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.marketkurly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketkurlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketkurlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/marketkurly/MarketkurlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marketkurly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketkurlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
