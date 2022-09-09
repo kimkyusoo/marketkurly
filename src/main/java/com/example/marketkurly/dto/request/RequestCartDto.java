8e266813010a1dd3a43343f6195e38633fd8d668
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-09-10 00:44:51 +0900

    1day

---
+++ src/main/java/com/example/marketkurly/dto/request/RequestCartDto.java
@@ -0,0 +1,14 @@
+package com.example.marketkurly.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestCartDto {
+    private ArrayList<Product> productId;
+}
